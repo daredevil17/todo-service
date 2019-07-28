bd1817e24f35654e869b9ba87e836d9c48e35584
daredevil17 <amehrotra75@gmail.com>
2019-07-28 10:07:28 -0700

    new todo-service

---
+++ README.md
@@ -0,0 +1,11 @@
+# bitbucket-repository-management-service
+#server start
+#go run cmd/grpc/server/server-todo-main.go -grpc-port=9090 -http-port=8080 -db-host=127.0.0.1:3306 -db-user=root -db-password=<pass> -db-schema=testdb
+
+#client rest test
+#cd bitbucket-repository-management-service/cmd/client-rest
+#go run main.go -server=http://localhost:8080
+
+#client grpc test
+#cd bitbucket-repository-management-service/cmd/grpc/client/
+#go run client-todo-main.go -server=localhost:9090
---
+++ build/proto-gen.sh
@@ -0,0 +1,16 @@
+export GO_PATH=$HOME/workspace/Go
+export PROTO_PATH=$GO_PATH/src/bitbucket-repository-management-service/dm/proto-files
+#add this path to ensure all protoc plugins can execute
+export PATH=$PATH:$GO_PATH/bin 
+
+protoc -I $GO_PATH/src --go_out=$GO_PATH/src $PROTO_PATH/domain/repository.proto
+protoc -I $GO_PATH/src --go_out=plugins=grpc:$GO_PATH/src $PROTO_PATH/service/repository-service.proto
+
+#TODO
+protoc -I $GO_PATH/src --proto_path=$PROTO_PATH/service:$PROTO_PATH/domain --go_out=plugins=grpc:$GO_PATH/src  --proto_path=third_party $PROTO_PATH/domain/todo.proto
+
+protoc -I $GO_PATH/src  --proto_path=$PROTO_PATH/service:$PROTO_PATH/domain --go_out=plugins=grpc:$GO_PATH/src --proto_path=third_party $PROTO_PATH/service/todo-service.proto
+
+protoc  --proto_path=$PROTO_PATH/service:$PROTO_PATH/domain --proto_path=third_party --grpc-gateway_out=logtostderr=true:$GO_PATH/src $PROTO_PATH/service/todo-service.proto
+
+protoc  --proto_path=$PROTO_PATH/service:$PROTO_PATH/domain  --proto_path=third_party --swagger_out=logtostderr=true:api/swagger/service $PROTO_PATH/service/todo-service.proto
